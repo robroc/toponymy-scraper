7c6450d8b100208aa1dd0f38cc67fb2a1d2b59b6
robroc <robroc1@gmail.com>
2015-05-15 09:55:01 +0800

    More fixes

--- toponymy.py
+++ toponymy.py
@@ -5,48 +5,59 @@ from __future__ import unicode_literals
 import time
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import StaleElementReferenceException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import requests
 from bs4 import BeautifulSoup
 import unicodecsv
 
+
 browser = webdriver.Chrome()
-f = open('toponymy_data.csv', 'wb+')
+f = open('toponymy_data.csv', 'ab+')
 writer = unicodecsv.writer(f)
 writer.writerow(["feature_name", "entry_date", "feature_type", "location", "history", "changed_name", "old_name","coords"])
 
 def main():
-
     browser.get('http://www.toponymie.gouv.qc.ca/ct/ToposWeb/recherche.aspx?avancer=oui')
     time.sleep(3)
     # Select Montreal only
     browser.find_element_by_xpath("//*[@id='ctl00_ConteneurToposWeb_lstRegAdm']/option[@value='06']").click()
     time.sleep(1)
     browser.find_element_by_xpath('//*[@id="ctl00_ConteneurToposWeb_btnSoumettreAvancer"]').click()
-    time.sleep(12)
+    time.sleep(20)
     table = browser.find_element_by_xpath('//*[@id="ctl00_ConteneurToposWeb_quadResultats"]/tbody')
     print "Scraping page 1"
     scrape_rows(table)
-        
+      
     for i in range (1,11):
-        print "Scraping page %s" % str(i+1)
-        browser.find_element_by_xpath('//*[@id="ctl00_ConteneurToposWeb_paginHaut"]/a[' + str(i) + ']').click()
-        time.sleep(12)
-        table = browser.find_element_by_xpath('//*[@id="ctl00_ConteneurToposWeb_quadResultats"]/tbody')
-        scrape_rows(table)
-           
+        paginate(i)
+
     for i in range (2, 7):
-        print "Scraping page %s" % str(i+10)
-        browser.find_element_by_xpath('//*[@id="ctl00_ConteneurToposWeb_paginHaut"]/a[' + str(i) + ']').click()
-        time.sleep(12)
-        table = browser.find_element_by_xpath('//*[@id="ctl00_ConteneurToposWeb_quadResultats"]/tbody')
-        scrape_rows(table)
-    
+        paginate(i)
+   
     f.close()
-    browser.close()
-    
+    browser.close() 
+ 
+def paginate(i):
+    print ""
+    print "SCRAPING PAGE %s" % str(i+1)
+    print ""
+    page_link =  WebDriverWait(browser, 12).until(EC.presence_of_element_located((By.ID,'ctl00_ConteneurToposWeb_paginHaut')))
+    browser.find_element_by_xpath('//*[@id="ctl00_ConteneurToposWeb_paginHaut"]/a[' + str(i) + ']').click()
+    time.sleep(30)
+    table = WebDriverWait(browser, 12).until(EC.presence_of_element_located((By.ID, 'ctl00_ConteneurToposWeb_quadResultats')))            
+    table = browser.find_element_by_xpath('//*[@id="ctl00_ConteneurToposWeb_quadResultats"]/tbody')
+    scrape_rows(table)
 
 def scrape_rows(table):
-    trs = table.find_elements_by_tag_name('tr')
+    row = 0
+    try:
+        trs = table.find_elements_by_tag_name('tr')
+    except StaleElementReferenceException:
+        time.sleep(10)
+        trs = table.find_elements_by_tag_name('tr')  
     for tr in trs[1:]:
         if tr.text != '':
             if tr.text[0].isdigit():
@@ -56,31 +67,34 @@ def scrape_rows(table):
                 id_start = link.find("=")+1
                 id_end = link.find("');")
                 page_id = str(link[id_start:id_end])
-                get_details(page_id)
+                row = row + 1  
+                get_details(page_id, row)
+        time.sleep(3)      
 
-def get_details(page_id):
+def get_details(page_id, row):
     changed_name = False
     old_name = "None"
     coords = ""
-    r = requests.get("http://www.toponymie.gouv.qc.ca/ct/ToposWeb/fiche.aspx?no_seq=" + page_id)
-    if r.status_code != 200:
-        time.sleep(5)
-        r = requests.get("http://www.toponymie.gouv.qc.ca/ct/ToposWeb/fiche.aspx?no_seq=" + page_id)
-    soup = BeautifulSoup(r.text)
-    entry_date = soup.find('span', attrs={'id':'ctl00_ConteneurToposWeb_vueFiche_etq17'}).text
-    feature_name = soup.find('span', attrs={'id':'ctl00_ConteneurToposWeb_vueFiche_etq19'}).text    
-    feature_type = soup.find('span', attrs={'id':'ctl00_ConteneurToposWeb_vueFiche_etq23'}).text
-    location = soup.find('span', attrs={'id':'ctl00_ConteneurToposWeb_vueFiche_etqMunicipalite'}).text
-    history = soup.find('span', attrs={'id':'ctl00_ConteneurToposWeb_vueFiche_etq9'}).text
-    old_name_div = soup.find('div', attrs={'id':'ctl00_ConteneurToposWeb_pTitreVariantes'})
-    coords_span = soup.find('span', attrs={'id':'ctl00_ConteneurToposWeb_vueFiche_Label3'})
-    if old_name_div is not None:
-        changed_name = True
-        old_name = soup.find('span', attrs={'id':'ctl00_ConteneurToposWeb_vueVariantes_ctl01_lbltopvar'}).text
-    if coords_span is not None:
-        coords = coords_span.text
-    print "Getting info for feature {0} {1}".format(feature_type, feature_name)
-    writer.writerow([feature_name, entry_date, feature_type, location, history, changed_name, old_name, coords])
+    headers = {'user-agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5); Roberto Rocha/robroc1@gmail.com'}
+    r = requests.get("http://www.toponymie.gouv.qc.ca/ct/ToposWeb/fiche.aspx?no_seq=" + page_id, headers=headers)
+    if r.status_code == 200:
+        soup = BeautifulSoup(r.text)
+        entry_date = soup.find('span', attrs={'id':'ctl00_ConteneurToposWeb_vueFiche_etq17'}).text
+        feature_name = soup.find('span', attrs={'id':'ctl00_ConteneurToposWeb_vueFiche_etq19'}).text    
+        feature_type = soup.find('span', attrs={'id':'ctl00_ConteneurToposWeb_vueFiche_etq23'}).text
+        location = soup.find('span', attrs={'id':'ctl00_ConteneurToposWeb_vueFiche_etqMunicipalite'}).text
+        history = soup.find('span', attrs={'id':'ctl00_ConteneurToposWeb_vueFiche_etq9'}).text
+        old_name_div = soup.find('div', attrs={'id':'ctl00_ConteneurToposWeb_pTitreVariantes'})
+        coords_span = soup.find('span', attrs={'id':'ctl00_ConteneurToposWeb_vueFiche_Label3'})
+        if old_name_div is not None:
+            changed_name = True
+            old_name = soup.find('span', attrs={'id':'ctl00_ConteneurToposWeb_vueVariantes_ctl01_lbltopvar'}).text
+        if coords_span is not None:
+            coords = coords_span.text
+        print "{0}. Fetching: {1} {2}".format(row, feature_type, feature_name)
+        writer.writerow([feature_name, entry_date, feature_type, location, history, changed_name, old_name, coords])
+    else:
+        print "Detail page not found"
     
 if __name__ == '__main__':
     main()
