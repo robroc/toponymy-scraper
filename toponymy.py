cdb69589933892ea5007fd0ae35028c1065ca420
robroc <robroc1@gmail.com>
2015-05-30 08:53:39 -0400

    final fixes

--- toponymy.py
+++ toponymy.py
@@ -15,9 +15,9 @@ import unicodecsv
 
 # Initialize Selenium webdriver and CSV writer as globals
 browser = webdriver.Chrome()
-f = open('toponymy_data.csv', 'ab+')
+f = open('toponymy_data_new.csv', 'ab+')
 writer = unicodecsv.writer(f)
-writer.writerow(["feature_name", "entry_date", "feature_type", "city", "borough", "history", "changed_name", "old_name","coords"])
+writer.writerow(["id","feature_name", "entry_date", "feature_type", "city", "borough", "history", "changed_name", "old_name","coords"])
 
 def main():
     browser.get('http://www.toponymie.gouv.qc.ca/ct/ToposWeb/recherche.aspx?avancer=oui')
@@ -27,12 +27,12 @@ def main():
     browser.find_element_by_xpath("//*[@id='ctl00_ConteneurToposWeb_lstRegAdm']/option[@value='06']").click()
     time.sleep(1)
     browser.find_element_by_xpath('//*[@id="ctl00_ConteneurToposWeb_btnSoumettreAvancer"]').click()
-    time.sleep(20)
+    time.sleep(10)
     
     # Start scraping the first page of results
     table = browser.find_element_by_xpath('//*[@id="ctl00_ConteneurToposWeb_quadResultats"]/tbody')
     print ""
-    pint "SCRAPING PAGE 1"
+    print "SCRAPING PAGE 1"
     print ""
     scrape_rows(table)
      
@@ -45,14 +45,14 @@ def main():
    
     f.close()
     browser.close() 
- 
+     
 def paginate(page):
     print ""
-    print "SCRAPING PAGE %s" % str(page+10)
+    print "SCRAPING PAGE %s" % str(page+1)
     print ""
     page_link =  WebDriverWait(browser, 12).until(EC.presence_of_element_located((By.ID,'ctl00_ConteneurToposWeb_paginHaut')))
     browser.find_element_by_xpath('//*[@id="ctl00_ConteneurToposWeb_paginHaut"]/a[' + str(page) + ']').click()
-    time.sleep(20)
+    time.sleep(10)
     table = WebDriverWait(browser, 12).until(EC.presence_of_element_located((By.ID, 'ctl00_ConteneurToposWeb_quadResultats')))           
     table = browser.find_element_by_xpath('//*[@id="ctl00_ConteneurToposWeb_quadResultats"]/tbody')
     scrape_rows(table)
@@ -88,8 +88,8 @@ def get_details(page_id, row):
         entry_date = soup.find('span', attrs={'id':'ctl00_ConteneurToposWeb_vueFiche_etq17'}).text
         feature_name = soup.find('span', attrs={'id':'ctl00_ConteneurToposWeb_vueFiche_etq19'}).text    
         feature_type = soup.find('span', attrs={'id':'ctl00_ConteneurToposWeb_vueFiche_etq23'}).text
-        city = soup.find('span', attrs={'id':'ctl00_ConteneurToposWeb_vueFiche_etqMunicipalite'}).text
-        borough = soup.find('span', attrs={'id':'ctl00_ConteneurToposWeb_vueFiche_etqFusion'}).text
+        city = soup.find('span', attrs={'id':'ctl00_ConteneurToposWeb_vueFiche_etqMunicipalite'}).text.replace(' (Ville)','')
+        borough = soup.find('span', attrs={'id':'ctl00_ConteneurToposWeb_vueFiche_etqFusion'}).text.replace('(','').replace(')','')
         history = soup.find('span', attrs={'id':'ctl00_ConteneurToposWeb_vueFiche_etq9'}).text
         old_name_div = soup.find('div', attrs={'id':'ctl00_ConteneurToposWeb_pTitreVariantes'})
         coords_span = soup.find('span', attrs={'id':'ctl00_ConteneurToposWeb_vueFiche_Label3'})
@@ -99,7 +99,7 @@ def get_details(page_id, row):
         if coords_span is not None:
             coords = coords_span.text
         print "{0}. Fetching: {1} {2}".format(row, feature_type, feature_name)
-        writer.writerow([feature_name, entry_date, feature_type, city, borough, history, changed_name, old_name, coords])
+        writer.writerow([page_id,feature_name, entry_date, feature_type, city, borough, history, changed_name, old_name, coords])
     else:
         print "Detail page not found"
     
